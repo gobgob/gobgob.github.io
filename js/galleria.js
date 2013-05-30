bb194fb30d220a028a554d3a9bbf8c17da041500
Fabien0102 <fabien.bernard@telecom-em.eu>
2013-05-30 20:25:52 +0200

    Oh my god ! It's a kitty :-O

--- js/galleria.js
+++ js/galleria.js
@@ -5,7 +5,8 @@ Galleria.loadTheme('js/galleria/themes/classic/galleria.classic.min.js');
 Galleria.run('#galleria', {
 
     // search flickr for "galleria"
-    flickr: 'user:96585783@N02',
+    //flickr: 'user:96585783@N02',
+    flickr: 'search:chatons',
     
     flickrOptions: {
         // sort by interestingness
